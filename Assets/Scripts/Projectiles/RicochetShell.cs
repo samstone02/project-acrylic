ff928031a698f45eeedcc505784b83ad9ddd7836
Sam Stone <samuelrystone@gmail.com>
2024-12-01 15:16:51 -0500

    ricochet projectile

--- Assets/Scripts/Projectiles/RicochetShell.cs
+++ Assets/Scripts/Projectiles/RicochetShell.cs
@@ -1,7 +1,51 @@
-﻿namespace Projectiles
+﻿using UnityEngine;
+
+namespace Projectiles
 {
     public class RicochetShell : Shell
     {
+        [field: SerializeField] public int RicochetCount { get; private set; }
         
+        [field: SerializeField] public float RicochetDetectionDistance { get; private set; }
+        
+        private Rigidbody _rigidbody;
+
+        protected void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+        
+        protected void Update()
+        {
+            bool reflected = ReflectIfRaycastHit();
+            
+            if (reflected)
+            {
+                RicochetCount--;
+            }
+            
+            if (RicochetCount == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected bool ReflectIfRaycastHit()
+        {
+            var ray = new Ray(transform.position, transform.forward);
+            Physics.Raycast(ray, out RaycastHit hitInfo, RicochetDetectionDistance, gameObject.layer);
+
+            if (hitInfo.collider == null)
+            {
+                return false;
+            }
+
+            Vector3 reflectionForward = Vector3.Reflect(transform.forward, hitInfo.normal);
+            float rotationAngle = 90 - Mathf.Atan2(reflectionForward.z, reflectionForward.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = new Vector3(0, rotationAngle, 0);
+            _rigidbody.velocity = _rigidbody.velocity.magnitude * reflectionForward;
+
+            return true;
+        }
     }
 }
